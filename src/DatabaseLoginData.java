61edceb58ce5835cfeb2742ac8ff77d1341b9660
SuneLindmark <sune.lindmark@elev.ga.ntig.se>
2020-05-12 09:20:56 +0200

    databaselogin tillagt

---
+++ src/DatabaseLoginData.java
@@ -0,0 +1,7 @@
+public class DatabaseLoginData {
+            static String user = "sune";
+            static String password = "slipkn0t";
+            static String port = "";
+            static String DBname = "";
+            static String DBURL = "";
+}
