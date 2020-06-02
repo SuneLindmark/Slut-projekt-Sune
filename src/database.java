0c54fce0b8de5a17f3a3ca3cafb1c69cd963aa8f
SuneLindmark <sune.lindmark@elev.ga.ntig.se>
2020-06-02 09:40:00 +0200

    mer arbete på databasen

--- src/database.java
+++ src/database.java
@@ -12,23 +12,18 @@ public class database {
             // Setup statement
             Statement stmt = conn.createStatement();
             // Create query and execute
-            String strSelect = "select body, description, qty from books";
-            System.out.println("The SQL statement is: " + strSelect + "\n");
+            String strSelect = "select * from story where id = 1";
+            //System.out.println("The SQL statement is: " + strSelect + "\n");
 
             ResultSet rset = stmt.executeQuery(strSelect);
 
             // Loop through the result set and print
 
             System.out.println("The records selected are:");
-            int rowCount = 0;
             while(rset.next()) {
                 String body = rset.getString("body");
-                String description = rset.getString("description");
-                int    qty   = rset.getInt("qty");
-                System.out.println(body + ", " + description + ", " + qty);
-                ++rowCount;
+                System.out.println(body);
             }
-            System.out.println("Total number of records = " + rowCount);
 
             // Close conn and stmt
             conn.close();
